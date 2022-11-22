3b4993c1e355064e742305c228747241f909fbca
weaponsforge <forgeweapons@gmail.com>
2022-11-22 13:21:35 +0800

    chore: Test using material-ui components

---
+++ components/ui/appcard/index.js
@@ -0,0 +1,16 @@
+import Card from '@mui/material/Card'
+import { styled } from '@mui/material/styles'
+
+const AppCard = styled(Card)(({ theme }) => ({
+  width: '100%',
+  padding: theme.spacing(2),
+  borderRadius: theme.spacing(1),
+  marginTop: theme.spacing(3),
+  boxShadow: `
+    rgb(255 255 255) 0px 0px 0px 0px,
+    rgb(0 0 0 / 5%) 0px 0px 0px 1px,
+    rgb(0 0 0 / 10%) 0px 10px 15px -3px, rgb(0 0 0 / 5%)
+    0px 4px 6px -2px !important`
+}))
+
+export default AppCard
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,11 @@
+import Typography from '@mui/material/Typography'
 import Section from '@/components/layout/section'
 import Navigation from '@/components/layout/navigation'
 
 function About () {
   return (
     <Section>
-      <h1>About</h1>
+      <Typography variant='h3'>About</Typography>
       <Navigation />
 
       <p>This is the About Page</p>
