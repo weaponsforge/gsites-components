cee3898952b2ecedcc18d964179673825fa34050
weaponsforge <forgeweapons@gmail.com>
2023-03-28 14:16:40 +0800

    Fix typo error

--- client/features/posts/index.js
+++ client/features/posts/index.js
@@ -2,7 +2,7 @@ import PostsList from './containers/list'
 import ViewPost from './containers/viewpost'
 import EditPost from './containers/editpost'
 import CreatePost from './containers/createpost'
-import useInitPosts from './hooks/useInitPosts'
+import useInitPosts from './hooks/useinitposts'
 
 export {
   PostsList,
