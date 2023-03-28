0f5788b8d98b48988c2cf7ae74151ce7af39ee0c
weaponsforge <forgeweapons@gmail.com>
2023-03-29 05:04:06 +0800

    chore: Use the fetchpost hook when viewing a Post

--- client/features/posts/containers/viewpost/index.js
+++ client/features/posts/containers/viewpost/index.js
@@ -1,11 +1,10 @@
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { useDispatch } from 'react-redux'
 
 import { useAuth } from '@/features/authentication'
-import { _getPost } from '@/store/posts/postThunks'
 import useDeletePost from '../../hooks/usedeletepost'
-import { MESSAGE_SEVERITY, notificationReceived } from '@/store/app/appSlice'
+import useFetchPost from '../../hooks/usefetchpost'
 
 import ViewPostComponent from '../../components/viewpost'
 
@@ -14,29 +13,18 @@ const defaultState = { isOpenDialog: false, docId: null, tempId: null }
 function ViewPost () {
   const [deleteState, setDeleteState] = useState(defaultState)
   const router = useRouter()
-  const mounted = useRef()
   const dispatch = useDispatch()
 
   const { authUser } = useAuth()
   const { deleteSuccess } = useDeletePost(authUser?.uid, deleteState.docId)
+  useFetchPost(authUser?.uid, deleteState.tempId)
 
   useEffect(() => {
     if (
       router.isReady &&
-      mounted.current === undefined &&
       authUser !== null
     ) {
-      mounted.current = true
-
-      dispatch(_getPost(`users/${authUser.uid}/posts/${router.query.id}`))
-        .unwrap()
-        .then()
-        .catch((error) => {
-          dispatch(notificationReceived({
-            notification: error ?? 'Error loading the post',
-            severity: MESSAGE_SEVERITY.ERROR
-          }))
-        })
+      setDeleteState(prev => ({ ...prev, tempId: router.query.id }))
     }
   }, [dispatch, router.isReady, router.query, authUser])
 
