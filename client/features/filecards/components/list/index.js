ead479eb6381a70dc3544dedfaf549448edae16c
weaponsforge <forgeweapons@gmail.com>
2023-04-08 15:58:35 +0800

    feat: Display the File Cards list, #85

--- client/features/filecards/components/list/index.js
+++ client/features/filecards/components/list/index.js
@@ -1,7 +1,39 @@
-import { HeaderNav } from '@/features/posts'
-import { SectionComponent } from '@/features/cms'
+import { useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
 
-function FileCardsComponent () {
+import {
+  DataGrid,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarContainer
+} from '@mui/x-data-grid'
+
+import { HeaderNav } from '@/features/posts'
+import AlertDialog from '@/components/common/ui/alertdialog'
+import { SectionComponent } from '@/features/cms'
+import { ADAPTER_STATES } from '@/store/constants'
+
+function CustomToolbar(props) {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+      <GridToolbarContainer {...props}>
+        <GridToolbarColumnsButton />
+        <GridToolbarFilterButton />
+      </GridToolbarContainer>
+    </div>
+  )
+}
+
+function FileCardsComponent ({
+  handleDeleteCancel,
+  handleDeleteConfirm,
+  deleteState,
+  deleteSuccess,
+  columns
+}) {
+  const {ids, entities: cards} = useSelector(state => state.cards)
+  const status = useSelector(state => state.cards.status)
+
   return (
     <SectionComponent>
       <HeaderNav
@@ -10,8 +42,50 @@ function FileCardsComponent () {
         buttonLink='/cms/filecards/create'
         buttonLabel='Create Card'
       />
+
+      <div style={{ height: 690, width: '100%', textAlign: 'left' }}>
+        <DataGrid
+          columns={columns}
+          rows={ids.map((id) => (cards[id]))}
+          loading={status === ADAPTER_STATES.PENDING}
+          components={{ Toolbar: CustomToolbar }}
+          maxColumns={4}
+          initialState={{
+            pagination: {
+              paginationModel: { pageSize: 10 }
+            }
+          }}
+          pageSizeOptions={[10, 20, 50]}
+        />
+      </div>
+
+      {/** Modal Dialog */}
+      {(deleteState.isOpenDialog) &&
+        <AlertDialog
+          isOpen={deleteState.isOpenDialog}
+          dialogTitle='Delete File Card'
+          dialogText={(deleteSuccess) ? 'File Card deleted.' : deleteState.message}
+          loading={status === ADAPTER_STATES.PENDING}
+          cancelCallback={handleDeleteCancel}
+          confirmCallback={() => {
+            if (deleteSuccess) {
+              handleDeleteCancel()
+            } else {
+              handleDeleteConfirm(deleteState?.tempId ?? null)
+            }
+          }}
+        />
+      }
     </SectionComponent>
   )
+}
+
+FileCardsComponent.propTypes = {
+  handleDeleteCancel: PropTypes.func,
+  handleDeleteConfirm: PropTypes.func,
+  deleteState: PropTypes.object,
+  deleteSuccess: PropTypes.bool,
+  columns: PropTypes.array
 }
 
 export default FileCardsComponent
--- client/features/filecards/containers/list/index.js
+++ client/features/filecards/containers/list/index.js
@@ -1,8 +1,78 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useRouter } from 'next/router'
+
+import { useAuth } from '@/features/authentication'
+import useDeleteCard from '../../hooks/usedeletecard'
+import { cardsReset } from '@/store/cards/cardSlice'
+
+import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
 import FileCardsComponent from '../../components/list'
 
+import DeleteForeverTwoToneIcon from '@mui/icons-material/DeleteForeverTwoTone'
+import PageviewTwoToneIcon from '@mui/icons-material/PageviewTwoTone'
+import BorderColorTwoToneIcon from '@mui/icons-material/BorderColorTwoTone'
+
+const defaultState = { isOpenDialog: false, docId: null, message: '', tempId: null }
+
 function FileCardsList () {
+  const [state, setState] = useState(defaultState)
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const { authUser } = useAuth()
+  const { deleteSuccess } = useDeleteCard(authUser?.uid, state.docId)
+
+  useEffect(() => {
+    // Delete the selected Card from cache
+    dispatch(cardsReset())
+  }, [dispatch])
+
+  const navigateToPostActionPage = (action, docId) => {
+    router.push(`/cms/filecards/${action}?id=${docId}`)
+  }
+
+  const handleDeleteIconClick = (docId) => {
+    setState({
+      ...state,
+      isOpenDialog: true,
+      tempId: docId,
+      docId: null,
+      message: `Do you want to delete File Card ${docId}?`
+    })
+  }
+
   return (
-    <FileCardsComponent />
+    <FileCardsComponent
+      handleDeleteCancel={() => setState(prev => ({ ...prev, isOpenDialog: false }))}
+      handleDeleteConfirm={(cardId) => setState(prev => ({ ...prev, docId: cardId }))}
+      deleteState={state}
+      deleteSuccess={deleteSuccess}
+      columns={[
+        { field: 'title', headerName: 'Title', minWidth: 250, flex: 1 },
+        { field: 'subtitle', headerName: 'Subtitle', minWidth: 180 },
+        { field: 'category', headerName: 'Category', minWidth: 190 },
+        { field: 'action', headerName: 'Action', minWidth: 150,
+          renderCell: (params) => {
+            return (
+              <ButtonGroup variant='outlined' size='small'>
+                <Button onClick={() => handleDeleteIconClick(params.id)}>
+                  <DeleteForeverTwoToneIcon />
+                </Button>
+                <Button onClick={() => navigateToPostActionPage('edit', params.id)}>
+                  <BorderColorTwoToneIcon />
+                </Button>
+                <Button onClick={() => navigateToPostActionPage('view', params.id)}>
+                  <PageviewTwoToneIcon />
+                </Button>
+              </ButtonGroup>
+            )
+          }
+        },
+        { field: 'date_created', headerName: 'Date Created', minWidth: 250 }
+      ]}
+    />
   )
 }
 
